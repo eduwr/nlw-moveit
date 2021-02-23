26b1af25f2fa8a06a7f51ce2a941f6af0274df04
eduwr <eduwric@gmail.com>
2021-02-23 19:41:32 -0300

    feat: :art: completed challenge

---
+++ src/components/Profile.tsx
@@ -0,0 +1,19 @@
+import styles from "../styles/components/Profile.module.css";
+
+export const Profile = () => {
+  return (
+    <div className={styles.profileContainer}>
+      <img
+        alt="eduwr"
+        src="https://avatars.githubusercontent.com/u/51808558?s=460&u=49ba9ae2dd4abb0062303df19a676a0a1e64d7eb&v=4"
+      />
+      <div>
+        <strong>Eduardo Wronscki</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level 1
+        </p>
+      </div>
+    </div>
+  );
+};
